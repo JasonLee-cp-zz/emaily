27943b13e2704183efa80a950314423df8ca51c4
Jason Lee <changjin9792@gmail.com>
2021-04-07 19:27:18 +0800

    email template styling

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -4,16 +4,17 @@ module.exports = (survey) => {
   return `
   <html>
     <body>
-      <div style="text-align: center;">
+      <div style="text-align: center; border: 1px solid lightgrey; background-color: lightyellow; padding: 3rem">
         <h3>I'd like your response!</h3>
-        <p>Please answer the foloowing question</p>
-        <p>${survey.body}</p>
-
         <div>
-          
-          <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
-          <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
-          
+          <p>Please answer the following question</p>
+          <p>${survey.body}</p>
+
+          <div>
+              <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes" style="text-decoration: none">Yes</a>
+              <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no" style="text-decoration: none">No</a>
+          </div>
+
         </div>
       </div>
     </body>
